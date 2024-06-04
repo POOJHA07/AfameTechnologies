6d28de4578f962b825009906971ac71398027d6a
POOJHA07 <poojha2123@gmail.com>
2024-06-04 19:10:45 +0530

    Added another section for introduction and changed some background colors

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ let buttons = document.querySelectorAll('button');
 let string = "";
 let arr = Array.from(buttons);
 
+
+// // script.js
+// const toggleButton = document.getElementById('toggleButton');
+
+// toggleButton.addEventListener('change', function() {
+//   if (this.checked) {
+//     console.log('Toggle button is ON');
+//     // Perform actions for ON state
+//   } else {
+//     console.log('Toggle button is OFF');
+//     // Perform actions for OFF state
+//   }
+// });
+
+
 arr.forEach(button => {
     button.addEventListener('click' , (e) =>{
 
